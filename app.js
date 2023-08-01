f3eb0bfac62baae158178b6cb6f55458e7a6dd85
GuillermoDev <joadex27@gmail.com>
2023-08-01 20:16:08 -0300

    Add page register & login

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (_req, res) => {
-  res.sendFile(resolve(__dirname, './views/home.html'));
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.listen(3000, () => {
