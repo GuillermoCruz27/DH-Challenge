270ce2ec1aa5df08dd1eafe6412e0c82956baf49
GuillermoDev <joadex27@gmail.com>
2023-08-08 20:00:56 -0300

    Fixed bug

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.get('/', (_req, res) => {
   res.sendFile(path.resolve(__dirname, './views/home.html'));
